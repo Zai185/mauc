bbb8d0e147aa5d8da03d1ab12ec09285378da5d3
Zai185 <86978374+Zai185@users.noreply.github.com>
2023-03-08 08:54:57 +0630

    remove one

--- src/components/Record.vue
+++ src/components/Record.vue
@@ -9,7 +9,7 @@
                     <h2>Take a look at our projects record</h2>
                 </div>
                 <div class="text-box-record projects valid">
-                    <h2>We have already finished<br>35 big projects</h2>
+                    <!-- <h2>We have already finished<br>35 big projects</h2> -->
                     <!-- <img class="text-box-img one" id="record-one"
                         src="https://cdn.home-designing.com/wp-content/uploads/2018/08/modern-platform-bedroom-sets.jpg"
                         alt="">
@@ -18,7 +18,7 @@
                         alt="">
                     <img class="text-box-img three" id="record-three"
                         src="https://images.pexels.com/photos/462235/pexels-photo-462235.jpeg?cs=srgb&dl=pexels-pixabay-462235.jpg&fm=jpg"
-                        alt=""> -->photo
+                        alt=""> -->
                 </div>
                 <div class="text-box-record class">
                     <h2>We have opened two classes</h2>
